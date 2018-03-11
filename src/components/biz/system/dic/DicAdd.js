99441117e406a6af2c53eeb02d62d21413743fc8
bvvy <bvvy@qq.com>
2018-03-11 18:02:27 +0800

    xiu gai 名称

---
+++ src/components/biz/system/dic/DicAdd.js
@@ -0,0 +1,108 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types'
+import {Form, Select, Input, Button, message} from 'antd';
+import axios from 'axios'
+
+
+const FormItem = Form.Item;
+
+const Option = Select.Option;
+
+class DicAdd extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+    handleSubmit = (e) => {
+        let self = this;
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+                axios.post('/v1/dic', values).then(res => {
+                    message.success(res.data.code);
+                    self.context.router.history.push('/dic/list')
+                })
+            }
+        });
+    };
+
+    handleSelectChange = (value) => {
+        console.log(value);
+        this.props.form.setFieldsValue({
+            note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+        });
+    };
+
+    render() {
+        const {getFieldDecorator} = this.props.form;
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormItem
+                    label="中文标签"
+                    labelCol={{span: 5}}
+                    wrapperCol={{span: 12}}
+                >
+                    {getFieldDecorator('cnLabel', {
+                        rules: [{required: true, message: '必填!'}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    label="英文标签"
+                    labelCol={{span: 5}}
+                    wrapperCol={{span: 12}}
+                >
+                    {getFieldDecorator('enLabel', {
+                        rules: [{required: true, message: '必填!'}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    label="组码"
+                    labelCol={{span: 5}}
+                    wrapperCol={{span: 12}}
+                >
+                    {getFieldDecorator('groupCode', {
+                        rules: [{required: true, message: '必填!'}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    label="组名"
+                    labelCol={{span: 5}}
+                    wrapperCol={{span: 12}}
+                >
+                    {getFieldDecorator('groupName', {
+                        rules: [{required: true, message: '必填!'}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    label="编码"
+                    labelCol={{span: 5}}
+                    wrapperCol={{span: 12}}
+                >
+                    {getFieldDecorator('code', {
+                        rules: [{required: true, message: '必填!'}],
+                    })(
+                        <Input/>
+                    )}
+                </FormItem>
+                <FormItem
+                    wrapperCol={{span: 12, offset: 5}}
+                >
+                    <Button type="primary" htmlType="submit">
+                        保存
+                    </Button>
+                </FormItem>
+            </Form>
+        );
+    }
+}
+
+
+export default DicAdd
---
+++ src/components/biz/system/dic/DicList.js
@@ -0,0 +1,76 @@
+import React, {Component} from 'react'
+import {Table, Icon, Divider, Popconfirm,Button} from 'antd';
+import axios from 'axios'
+import {Link} from 'react-router-dom'
+
+class DicList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            columns: [{
+                title: '序号',
+                dataIndex: 'id',
+                render: text => <a href="#">{text}</a>
+            }, {
+                title: '中文标签',
+                dataIndex: 'cnLabel'
+            }, {
+                title: '英文标签',
+                dataIndex: 'enLabel'
+            }, {
+                title: '组码',
+                dataIndex: 'groupCode'
+            }, {
+                title: '组名',
+                dataIndex: 'groupName'
+            }, {
+                title: '编码',
+                dataIndex: 'code'
+            }, {
+                title: '操作',
+                key: 'action',
+                render: (text, record) => (
+                    <span>
+                <Link to={`/dic/update/${record.id}`}>修改</Link>
+            <Divider type="vertical"/>
+            <Popconfirm title="确定删除吗?" onConfirm={() => this.onDelete(record.id)}>
+              <a href="#">删除</a>
+            </Popconfirm>
+        </span>
+                )
+            }]
+        }
+    }
+
+
+    onDelete = (id) => {
+        let self = this;
+        axios.delete(`/v1/dic/${id}`).then((res) => {
+            console.log(res);
+            self.all();
+        })
+    };
+
+    componentDidMount() {
+        this.all();
+    }
+
+    all = () => {
+        let self = this;
+        axios.get('/v1/dic/all').then((res) => {
+            self.setState({data: res.data})
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <Button ><Link to="/dic/add">添加</Link></Button>
+                <Table rowKey="id" columns={this.state.columns} dataSource={this.state.data}/>
+            </div>
+        )
+    }
+}
+
+export default DicList;
